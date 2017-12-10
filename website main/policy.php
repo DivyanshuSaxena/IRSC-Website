b1e64685050fd68c9b60553a28936a92c23aa507
shikharmakker <shikharmakker@gmail.com>
2017-12-11 00:27:14 +0530

    policy update

--- website main/policy.php
+++ website main/policy.php
@@ -151,12 +151,167 @@ of Dada Dev Hospital."</td>
                     </tr>
 
                     <tr>
-                        
+                        <td rowspan="4">Burari By-pass,Nirankari Sarovar Delhi</td>
+                        <td>"No traffic signs (signals) for the purpose of
+traffic regulation"</td>
+                        <td rowspan="2">Construction of roundabouts or U-turns synchronized with traffic lights at the 4-way intersection to regulate the heavy and fast-moving traffic comuting to and fro the outer ring road.</td>
+                    </tr>
+                    <tr>
+                        <td>"No restrictions imposed on public transport
+like GraminSewa , RTV etc."</td>
+                    </tr>
+                    <tr>
+                        <td>Problem of cows and stray animals:</td>
+                        <td>Shifting of garbage dumping point</td>
+                    </tr>
+                    <tr>
+                        <td>Problem of Divider cuts (U-turns)</td>
+                        <td>Reducing number of divider cuts on the main Burari Road</td>
+                    </tr>
+
+                    <tr>
+                        <td rowspan="5">Sri Aurobindo Marg (near AIIMS and Safdarjung Hospital) and the AIIMS Flyover</td>
+                        <td rowspan="2">Accumulation of Rainwater during Monsoons</td>
+                        <td>Subway Immediate to the entrance of AIIMS</td>
+                    </tr>
+                    <tr>
+                        <td>"Removal of traffic lights and Construction of Grills on the divider on Sri Aurobindo
+Marg"</td>
+                    </tr>
+                    <tr>
+                        <td rowspan="3">Obstruction caused by Auto-Stands due to Oncoming Traffic</td>
+                        <td>Construction of a Foot-over Bridge in front of AIIMS(Gate no. 1 & 2)</td>
+                    </tr>
+                    <tr>
+                        <td>Implementation of a proper auto-stand which won’t hinder the flow of traffic</td>
+                    </tr>
+                    <tr>
+                        <td>Proper storm water drainage system in the AIIMS underpass</td>
+                    </tr>
+
+                    <tr>
+                        <td rowspan="3">P and T circle, Jodhpur, Rajasthan.</td>
+                        <td>"Crossing at the P and T circle, which contains a blind turn which is
+a cause for trouble for the local residents."</td>
+                        <td>"Install a mirror on the circle, so that the people
+could see the car coming from the other side well before the car was
+actually visible."</td>
+                    </tr>
+                    <tr>
+                        <td>"Tall boundary walls restrict the visibility of the driver of the vehicles, causing a great number
+of accidents."</td>
+                        <td>"There should be
+speed breakers made on the road to slow down the cars coming from
+either side,"</td>
+                    </tr>
+                    <tr>
+                        <td>"No rules as such being violated, but this blind turn is causing a problem for many
+people who cross this area daily."</td>
+                        <td>"A traffic policeman should be present on the circle, so that people do
+not drive on the wrong side."</td>
+                    </tr>
+
+                    <tr>
+                        <td rowspan="4">Shahbad Daulatpur to Bawana road, Delhi</td>
+                        <td>No proper traffic signals and aministration at Bawana Chowk(3- way junction) leading to severe traffic jams.</td>
+                        <td>At the Bawana Chowk the bus stand should be shifted a bit away from the junction.</td>
+                    </tr>
+                    <tr>
+                        <td>No traffic police at the Mahadev chowk(4-way junction) amd no pedestrian crossings.</td>
+                        <td>At Mahadev Chowk proper traffic lights should be installed, traffic officials should be placed there,leveling of the road should be done, pedestrian crossings,blinkers and lane markings are required.</td>
+                    </tr>
+                    <tr>
+                        <td>The area near Shahbad dairy is used as a bus stand for all buses and RTVs leading to traffic congestion and accidents.</td>
+                        <td>At Shahbad Dairy separate lanes should be constructed for school buses and pedestrians. Illegal parkings should be removed</td>
+                    </tr>
+                    <tr>
+                        <td>No divider or speed breakers nearby DTU Gate to avoid chaos at peak times.</td>
+                        <td>Speed breakers and dividers should be constructed near the DTU gate and stoppage of vehicles right in front og the gate should be avoided.</td>
+                    </tr>
+
+                    <tr>
+                        <td rowspan="4">Block 6 Raja Ram Kohli Marg, across Ramleela ground, Police Station, Geeta Colony.</td>
+                        <td rowspan="4">Raja Ram Kholi Marg is an accident prone area. It has a T-point without any traffic signals(red light) and dividers on the road.</td>
+                        <td>A red light should be installed at the T-point.</td>
+                    </tr>
+                    <tr><td>Zebra crossings should be made for passe byes.</td></tr>
+                    <tr><td>Footpaths crowded with vehicles and automobiles should be penalized.</td></tr>
+                    <tr><td>A thin divider shouldbe constructed on the road.</td></tr>
+
+                      <tr>
+                        <td rowspan="5">Shahdara Road underTraffic lights should be made functional.  flyover near Shyamlal College</td>
+                        <td>Unauthorized parking by local people.</td>
+                        <td>Traffic lights should be made functional. </td>
+                    </tr>
+                    <tr>
+                        <td>DTC buses cause major traffic congestions.</td>
+                        <td>Traffic officials should be appointed.</td>
+                    </tr>
+                    <tr>
+                        <td>Road becomes narrower as we move forward because of timber shops.</td>
+                        <td>Encroachment of road by vendors and shops should be stopped.</td>
+                    </tr>
+                    <tr>
+                        <td>From Durgapuri till Shahdara flyover there is no divider present.</td>
+                        <td>Proper divider should be constructed.</td>
+                    </tr>
+                    <tr>
+                        <td>Auto Rickshaws move in wrong lanes.</td>
+                        <td>Proper auto stands should be constructed.</td>
+                    </tr>
+
+                      <tr>
+                        <td>Ajmer road, Jaipur</td>
+                        <td>The Ajmer Road junction is a busy intersection which has traffic congestion problrms.There are no proper traffic officials and traffic signals at the junction.Mismanagement in the area leads to serious accidents.</td>
+                        <td>A separate flyover for buses and bigger four wheelers.Enforcement of traffic light use. Checking the negligence of traffic police. Shifting of nearby shops which attract crowd mainly(bar and liquor shops).</td>
+                    </tr>
+
+                      <tr>
+                        <td rowspan="3">Kosi Kalan, Mathura, Uttar Pradesh, India</td>
+                        <td>Some of the major roads of the town, connecting the town with main higways and interconnecting prime localities of the town are in worst conditions resulting in major accidents</td>
+                        <td>Construction of roads at right angles,proper sewage systems, dividers, clear sidewalks, and roundabouts in popolated areas like: Agrasen Chowk and Sherparh road</td>
+                    </tr>
+                    <tr>
+                        <td>Local people are unaware of rules and regulations related to road safety</td>
+                        <td>Spread awareness through various programmes</td>
+                    </tr>
+                     <tr>
+                        <td>Weekly market of animal trading brings heavy vehicles to the are leading to severe traffic jams</td>
+                        <td>Employment of Traffic Officials near Ghanta Ghar and contact the school authorities to manage the timings and routes of the school buses</td>
+                    </tr>
+
+                      <tr>
+                        <td rowspan="4">Kattur Junction to Palpannai Junction on NH-67, Tiruchirappalli District, Tamil Nadu, India.</td>
+                        <td>The are between Kattur Junction and Palpannai Junction has a lot of black spots.</td>
+                        <td rowspan="2">At the Oil Mill Junction, a flyover can be constructed.This will allow the vehicles to pass through the underpass and the U-turn will also be accommodated.The traffic can move freely without the interference of the pedesrians.</td>
+                    </tr>
+                    <tr>
+                        <td>At the Ariyamangalam Oil MIll Junction, there are no service roads and side roads, no reserve lanes, no U-turns, no traffic signals. There are commercial shops encouraging local traffic, public and institutional buildings.</td>
+                    </tr>
+                    <tr>
+                        <td>At the Seshasayee Institute of Technology Junction, there are no service and side roads, no U-turns and no street lights.there are also institutional buildings, commercial traffic is prominent and number of right turning vehicles are more.</td>
+                        <td rowspan="2">At the SIT Junction, Service lanes can be provided for the approach road on either side. The median should be joined in the intersection prohibiting U and right turning vehicles. A Foot-over-bridge should also be constructed for the pedestrians.</td>
+                    </tr>
+                    <tr>
+                        <td>There is a lack of proper pedestrian walkways and crossings.Animals are wandering on te main road.There are unused bus bays. Overspeeding is prominent. Information signs ae in local language.</td>
+                    </tr>
+
+                     <tr>
+                        <td rowspan="4">Britannia Chowk to Punjabi Bagh Chowk, Delhi, India</td>
+                        <td rowspan="2">At the Britannia Chowk, there are no Zebra crossing, Insufficient lighting is there, bad road design and pedestrian line violation.</td>
+                        <td>Redesigning of Punjabi Bagh Chowk with better traffic contol facilities.</td>
+                    </tr>
+                    <tr>
+                        <td>Providing better street furniture for people, Bollards with lights can be installed at the Britannia Chowk, Life saving rolling barriers and street lights should also be installed.</td>
+                    </tr>
+                    <tr>
+                        <td rowspan="2">At the Punjabi Bagh Chowk, there are no Zebra crossings, no footpaths on roundabouts, bad road geometric red light roundabout, and a closed subway.</td>
+                        <td>3D zebra crossings, places for the disabled, yellow line markings and road signs on dedicated lanes are required.</td>
+                    </tr>
+                    <tr>
+                        <td>Red light violation detection systems, radar controlled roads speed cameras and digital sign boards are some other solutions.</td>
                     </tr>
                     
-
-
-
                  </table>
 
 
